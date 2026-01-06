04dbe1cf9d0a342cf58dd3bce8d76ca29ce254cb
NULL882 <gost6865@yandex.ru>
2026-01-06 16:49:20 +0300

    optimization Z-levels

--- Content.Server/_SCP/PVS/CEPvsOverrideSystem.cs
+++ Content.Server/_SCP/PVS/CEPvsOverrideSystem.cs
@@ -12,16 +12,16 @@ public sealed partial class CEPvsOverrideSystem : EntitySystem
     [Dependency] private readonly PvsOverrideSystem _pvs = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<CEPvsOverrideComponent, ComponentStartup>(OnLighthouseStartup);
-        SubscribeLocalEvent<CEPvsOverrideComponent, ComponentShutdown>(OnLighthouseShutdown);
+        SubscribeLocalEvent<CEPvsOverrideComponent, ComponentStartup>(OnPvsStartup);
+        SubscribeLocalEvent<CEPvsOverrideComponent, ComponentShutdown>(OnPvsShutdown);
     }
 
-    private void OnLighthouseShutdown(Entity<CEPvsOverrideComponent> ent, ref ComponentShutdown args)
+    private void OnPvsShutdown(Entity<CEPvsOverrideComponent> ent, ref ComponentShutdown args)
     {
         _pvs.RemoveGlobalOverride(ent);
     }
 
-    private void OnLighthouseStartup(Entity<CEPvsOverrideComponent> ent, ref ComponentStartup args)
+    private void OnPvsStartup(Entity<CEPvsOverrideComponent> ent, ref ComponentStartup args)
     {
         _pvs.AddGlobalOverride(ent);
     }
